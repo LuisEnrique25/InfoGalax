767b29cdb2212fe2256b7363a6bea54f3e800514
LuisEnrique25 <luisrg2225@gmail.com>
2023-05-21 00:05:39 -0600

    Arregle algunos problemas que rompian el diseño responsive mediante renderizacion condicional

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Entregable 1 de la primera semana del modulo de React.
 Principalmente se usa useState para cambiar frases y fondo de manera aleatoria cada vez que se recarga la pagina o mediante un botón. 
 
 
-<h2>Projecto desplegado en: <b>galaxy-phrases.netlify.app</b></h2>
+<h2>Projecto desplegado en: <span style="color: blue; font-weight: bold;" >galaxy-phrases.netlify.app</span></h2>
--- src/components/PhraseButton.jsx
+++ src/components/PhraseButton.jsx
@@ -3,12 +3,16 @@ import "./phraseButton.css"
 import Data from '../db/quote.json'
 
 const PhraseButton = ({random, changePhrase}) => {
+  
+  const rulePhrase = (num) =>{
+    if(num === 4 || num === 9 || num === 5) return true;
+}  
   return (
     <div className='phrase'>
-        <div className="phrase__container">
+        <div className={`phrase__container ${rulePhrase(random) && "small_font"}`}> 
           <p>{Data[random].phrase}</p>
         </div>
-        <button onClick={changePhrase}  className='btn__reload'><i class="fa-solid fa-rotate-right"></i></button>
+        <button onClick={changePhrase}  className="btn__reload" ><i class="fa-solid fa-rotate-right"></i></button>
         
     </div>
   )
